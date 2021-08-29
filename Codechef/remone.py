71116b0d7050d08f5d7096e49e9f6c698479ca7f
anshyyy <anshuman9998@gmail.com>
2021-08-29 19:07:12 +0530

    FILES

--- Codechef/remone.py
+++ Codechef/remone.py
@@ -1,39 +1,24 @@
-#include <bits/stdc++.h>
-#define IOS ios_base::sync_with_stdio(false); cin.tie(NULL);
-#define loop(i,n) for(int i=0;i<n;i++)
-#define for1(i,n) for(int i=1;i<=n;i++)
-typedef long long ll;
-using namespace std;
-void solve(){ 
-    ll n;
-    cin>>n;
-    vector<int>arr(n),brr(n-1);
-    loop(i,n)cin>>arr[i];
-    loop(i,n-1)cin>>brr[i];
-    vector<ll> freq(10000,0);
-    unordered_map<ll,ll> map;
-    for(int i=0;i<n;i++){
-        for (int j = 0; j <n-1; j++)
-        {
-             map[brr[i]-arr[j]]++;
-        } 
-    }
-    ll ans=INT_MIN;
-    for(auto x : map){
-        if(x.second>ans){
-            ans=x.first;
-        }
-    }
-     for (auto x : map)
-      cout << x.first << " " << x.second << endl;
-    
-    cout<<ans<<endl;
-}
-int main() {
-int t=1;
-cin>>t;
-while(t--){
- solve(); 
-}
-     return 0;
-}
+from collections import *
+for i in range(int(input())):
+    n=int(input())
+    a=list(map(int,input().split()))
+    b=list(map(int,input().split()))
+    x=float("inf")
+    a.sort()
+    b.sort()
+    m=defaultdict(int)
+    for i in range(n-1):
+        m1=b[i]-a[i]
+        m2=b[i]-a[i+1]
+        if m1!=m2:
+            if m1>0:
+                m[m1]+=1
+            if m2>0:
+                m[m2]+=1
+        else:
+            if m1>0:
+                m[m1]+=1
+    for i in m.keys():
+        if m[i]==n-1:
+            x=min(x,i)  
+    print(x)
