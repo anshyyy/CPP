f3b91c7bbcb17b377edd1c1b03bce9baf3cd69a2
anshyyy <anshuman9998@gmail.com>
2022-04-29 21:48:03 +0530

    FILES

---
+++ Codeforces/1646C.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define IOS                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define loop(i, a, n) for (int i = a; i < n; i++)
+#define all(v) (v).begin(), (v).end()
+#define all1(v) (v).begin() + 1, (v).end()
+#define allr(v) (v).rbegin(), (v).rend()
+#define allr1(v) (v).rbegin() + 1, (v).rend()
+#define ff first
+#define ss second
+typedef long long ll;
+const ll INF = 1e9 + 7;
+const ll MOD = 998244353;
+using namespace std;
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n + 1), b(n + 1);
+    int sum1 = 0, sum2 = 0;
+    loop(i, 1, n + 1)
+    {
+        cin >> arr[i];
+        sum1 += arr[i];
+    }
+    loop(i, 1, n + 1)
+    {
+        cin >> b[i];
+    }
+    int ans = 1e9;
+    for (int i = 1; i <= n; i++)
+    {
+        sum1 -= arr[i];
+        ans = min(ans, max(sum1, sum2));
+        sum2 += b[i];
+    }
+    cout << ans << endl;
+}
+int main()
+{
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
