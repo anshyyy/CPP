3464893b325bf7324f3cecced40dc44ff9297cdd
anshyyy <anshuman9998@gmail.com>
2022-07-02 21:12:00 +0530

    FILES

---
+++ Codeforces/219B.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define IOS ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define loop(i,a,n) for(int i=a;i<n;i++)
+#define all(v) (v).begin(),(v).end()
+#define all1(v) (v).begin()+1,(v).end()
+#define allr(v) (v).rbegin(),(v).rend()
+#define allr1(v) (v).rbegin()+1,(v).rend()
+#define ff first
+#define ss second
+typedef long long ll;
+const ll INF = 1e9+7;
+const ll MOD = 998244353;
+using namespace std;
+void solve(){ 
+     ll p,d;cin>>p>>d;
+     ll r = 10;
+     ll ans = ++p;
+     while(p%r<=d){
+        ans=p-p%r;
+        r*=10;
+     }
+     cout<<ans-1<<endl;
+}
+int main() {
+IOS;
+int t=1;
+cin>>t;
+while(t--){
+ solve(); 
+}
+     return 0;
+}
